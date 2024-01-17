590da5f6ad63a716f17a7c2b2b18b2094100fcdb
iankohhh <iankoh@amazon.com>
2024-01-18 02:14:33 +0800

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -21,3 +21,27 @@ function downloadFile() {
         })
         .catch(error => console.error('Error:', error));
 }
+
+function downloadFile() {
+    // Replace 'your-api-endpoint' with the actual URL of your API Gateway endpoint
+    var apiEndpoint = 'https://ja9vw9anx2.execute-api.ap-southeast-1.amazonaws.com/test/cheatsheet';
+
+    // Make a GET request to the API endpoint
+    fetch(apiEndpoint)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.text();
+        })
+        .then(url => {
+            // Create a hidden link and trigger the download
+            var link = document.createElement('a');
+            link.href = url;
+            link.download = 'cheatsheet.pdf'; // Set the desired filename
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        })
+        .catch(error => console.error('Error:', error));
+}
