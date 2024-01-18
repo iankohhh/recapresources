779471bb1d5bf5eada73117ccd198dd0d4443287
iankohhh <iankoh@amazon.com>
2024-01-18 19:01:43 +0800

    Updated for exec slides

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,34 @@
-function downloadFile() {
+function downloadBuilderSlides() {
     // Replace 'your-api-endpoint' with the actual URL of your API Gateway endpoint
-    var apiEndpoint = 'https://ja9vw9anx2.execute-api.ap-southeast-1.amazonaws.com/test/builder';
+    var apiEndpoint = 'https://xlzxqbhq6g.execute-api.ap-southeast-1.amazonaws.com/dev/download/recapbuilderslides';
+
+    // Make a GET request to the API endpoint
+    fetch(apiEndpoint)
+        .then(response => {
+            console.log(response);
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json(); // Parse the response as JSON
+        })
+        .then(data => {
+            // Extract the URL directly from the "body" property
+            var url = data.body;
+
+            // Create a hidden link and trigger the download
+            var link = document.createElement('a');
+            link.href = url;
+            link.download = 'recapBuilderSlides.zip'; // Set the desired filename
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+function downloadExecSlides() {
+    // Replace 'your-api-endpoint' with the actual URL of your API Gateway endpoint
+    var apiEndpoint = 'https://xlzxqbhq6g.execute-api.ap-southeast-1.amazonaws.com/dev/download/recapexecslides';
 
     // Make a GET request to the API endpoint
     fetch(apiEndpoint)
@@ -28,7 +56,7 @@ function downloadFile() {
 
 function downloadCheatsheet() {
     // Replace 'your-api-endpoint' with the actual URL of your API Gateway endpoint
-    var apiEndpoint = 'https://ja9vw9anx2.execute-api.ap-southeast-1.amazonaws.com/test/cheatsheet';
+    var apiEndpoint = 'https://xlzxqbhq6g.execute-api.ap-southeast-1.amazonaws.com/dev/download/cheatsheet';
 
     // Make a GET request to the API endpoint
     fetch(apiEndpoint)
